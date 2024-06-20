7daab0721c16c0e9cadca9c3ab8709e184eacb38
Gustavo Mansur <gurmansur@gmail.com>
2024-06-20 12:24:42 -0300

    setup: update to angular 18

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AuthModule,
-  ],
-  providers: [
-    { provide: 'servicesRootUrl', useValue: 'http://localhost:3333' },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        AuthModule], providers: [
+        { provide: 'servicesRootUrl', useValue: 'http://localhost:3333' },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
