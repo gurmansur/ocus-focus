ce30db829490597b2247ef9f1514a1ad1f2b55ee
Gustavo Mansur <gurmansur@gmail.com>
2024-06-20 21:13:12 -0300

    Revert "setup: update to angular 18"
    This reverts commit 7daab0721c16c0e9cadca9c3ab8709e184eacb38.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 
-@NgModule({ declarations: [AppComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        AuthModule], providers: [
-        { provide: 'servicesRootUrl', useValue: 'http://localhost:3333' },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule,
+  ],
+  providers: [
+    { provide: 'servicesRootUrl', useValue: 'http://localhost:3333' },
+  ],
+  bootstrap: [AppComponent],
+})
 export class AppModule {}
