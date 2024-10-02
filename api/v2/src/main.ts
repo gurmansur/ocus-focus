00919f09f51fa223cb7e41356599ce9153756b75
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-10-01 22:07:12 -0300

    feat(Geral): 🚀 teste para uso da Swagger API

--- api/v2/src/main.ts
+++ api/v2/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,20 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Ocus Focus')
+    .setDescription('Documentação da API do projeto Ocus Focus')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+  });
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
