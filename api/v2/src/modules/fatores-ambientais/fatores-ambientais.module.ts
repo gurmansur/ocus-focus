abb1441c7e8272e239ec8274351e03124c57838e
Gustavo Mansur <gurmansur@gmail.com>
2024-10-12 21:10:21 -0300

    refactor(Geral): :fire: removidos endpoints não utilizados

--- api/v2/src/modules/fatores-ambientais/fatores-ambientais.module.ts
+++ api/v2/src/modules/fatores-ambientais/fatores-ambientais.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FatorAmbiental } from './entities/fatores-ambientais.entity';
-import { FatoresAmbientaisController } from './fatores-ambientais.controller';
 import { FatoresAmbientaisService } from './fatores-ambientais.service';
 
 @Module({
-  controllers: [FatoresAmbientaisController],
   providers: [FatoresAmbientaisService],
   imports: [TypeOrmModule.forFeature([FatorAmbiental])],
 })
