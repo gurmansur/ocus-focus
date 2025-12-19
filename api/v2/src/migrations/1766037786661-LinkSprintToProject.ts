8885fa962de4d640ea4eeda5205f24a23dbf4890
Gustavo Mansur <gurmansur@gmail.com>
2025-12-19 01:08:48 -0300

    style: format migration code for consistency

--- api/v2/src/migrations/1766037786661-LinkSprintToProject.ts
+++ api/v2/src/migrations/1766037786661-LinkSprintToProject.ts
@@ -1,16 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class LinkSprintToProject1766037786661 implements MigrationInterface {
-    name = 'LinkSprintToProject1766037786661'
+  name = 'LinkSprintToProject1766037786661';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`SPRINTS\` ADD \`FK_PROJETOS_PRO_ID\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`SPRINTS\` ADD CONSTRAINT \`FK_df9b4d76871e6c4d68abfed4295\` FOREIGN KEY (\`FK_PROJETOS_PRO_ID\`) REFERENCES \`PROJETOS\`(\`PRO_ID\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`SPRINTS\` ADD \`FK_PROJETOS_PRO_ID\` int NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`SPRINTS\` ADD CONSTRAINT \`FK_df9b4d76871e6c4d68abfed4295\` FOREIGN KEY (\`FK_PROJETOS_PRO_ID\`) REFERENCES \`PROJETOS\`(\`PRO_ID\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`SPRINTS\` DROP FOREIGN KEY \`FK_df9b4d76871e6c4d68abfed4295\``);
-        await queryRunner.query(`ALTER TABLE \`SPRINTS\` DROP COLUMN \`FK_PROJETOS_PRO_ID\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`SPRINTS\` DROP FOREIGN KEY \`FK_df9b4d76871e6c4d68abfed4295\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`SPRINTS\` DROP COLUMN \`FK_PROJETOS_PRO_ID\``,
+    );
+  }
 }
